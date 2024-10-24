42ae08333bce07805ea5907215dc0b5be399762e
qinbo.zhang <zhangqinbo@light2cloud.com>
2024-10-24 14:23:46 +0800

    删除 AccessKey 信息

--- models/util/AccessKey.go
+++ models/util/AccessKey.go
@@ -9,43 +9,43 @@ type IdAndSecret struct {
 
 var Key = map[string]IdAndSecret{
 	"1": {
-		ID:        "LBSnKHUvFYIwzHsce18BMqsbKyzLPZ5xiB18kFgSAnc=",
+		ID:        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=",
 		Secret:    "GMJk9z5r881ZKPvtvzCR3rE2AXK2z/ug5B/asgpK7wA=",
 		AccountId: "1",
 		Name:      "zb", //总部
 	},
 	"2": {
-		ID:        "MI58WUTio0ECCioErhQ/p3F757kbAZfc0QMURF/otKg=",
+		ID:        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=",
 		Secret:    "3bPK5PrwhU6Z+fjhOkF75Hrc4yekPR2KNRA6lR6GIbo=",
 		AccountId: "2",
 		Name:      "gj", //管家
 	},
 	"3": {
-		ID:        "taQW+3qRXs61Bno9k15y4DlUVrJoq4Axr2p5ngjDmk0=",
+		ID:        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=",
 		Secret:    "lSZWEyOG5o1xUZk+SGXpm/SgQPiyZnComWPAxNZnLNc=",
 		AccountId: "3",
 		Name:      "st", //生泰
 	},
 	"4": {
-		ID:        "MgSYFn5nC3qD027gTRCqEKV82yFkzdfRzd7aHZ1l7B4=",
+		ID:        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=",
 		Secret:    "1DY0wKV1VYKt+tNYGCpLP40aT+Dpa7AY9mxgAV2f25k=",
 		AccountId: "4",
 		Name:      "xx", //信息
 	},
 	"5": {
-		ID:        "jYvDT9Su0MX1LnGi6kLMTTrRGREUgDssSvJOu7AzW34=",
+		ID:        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=",
 		Secret:    "PZ+G0ua+b6R0qXRo1xLengriUneiT6r6Dn1sAD4uO20=",
 		AccountId: "5",
 		Name:      "sc", // 市场
 	},
 	"6": {
-		ID:        "iH2t6+Ep+QH91GLFQmKmZleDP9y5hBMzoZvyH8zCSTE=",
+		ID:        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=",
 		Secret:    "CoYhbLgv8Q9ccQOQjAIxpfgeO0XwjX+q/3Mfk/mqxyA=",
 		AccountId: "6",
 		Name:      "stOld", // 旧机电
 	},
 	"7": {
-		ID:        "SIGIX6VILU3F+9YutT2h19IpeddamRP80Y/fFYhhcG8=",
+		ID:        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=",
 		Secret:    "bV9WFRyJ21dES3EPXZn9uNM7Ki84p9GW7j7B3KIveII=",
 		AccountId: "7",
 		Name:      "gjOld", // 旧管家
